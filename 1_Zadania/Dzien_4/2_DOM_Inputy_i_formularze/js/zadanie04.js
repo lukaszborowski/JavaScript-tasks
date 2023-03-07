5776a6af5a79ac41c2a86419a9fdcbcc5b53354b
lukaszborowski <lukasz1borowski@gmail.com>
2023-03-07 21:48:03 +0100

    added regex

--- 1_Zadania/Dzien_4/2_DOM_Inputy_i_formularze/js/zadanie04.js
+++ 1_Zadania/Dzien_4/2_DOM_Inputy_i_formularze/js/zadanie04.js
@@ -8,34 +8,39 @@ document.addEventListener("DOMContentLoaded", function() {
 
     cardNumber.addEventListener("input", function (){
 
+        const numberTest = /^[0-9]*$/;
         cardNumber.style.borderColor = "red";
-
-            if(cardNumber.value.charAt(0) === "4" && cardNumber.value.length >= 13 && cardNumber.value.length <= 16){
+        if(numberTest.test(cardNumber.value)) {
+            if (cardNumber.value.charAt(0) === "4" && cardNumber.value.length >= 13 && cardNumber.value.length <= 16) {
                 cardNumber.style.borderColor = "green";
                 cardType.textContent = "visa";
                 btn.disabled = false;
-            }else if (cardNumber.value.length > 16){
+            } else if (cardNumber.value.length > 16) {
                 cardType.textContent = "";
                 cardNumber.style.borderColor = "red";
                 btn.disabled = true;
-            } else if(cardNumber.value.charAt(0) === "5" && cardNumber.value.length === 16){
+            } else if (cardNumber.value.charAt(0) === "5" && cardNumber.value.length === 16) {
                 cardNumber.style.borderColor = "green";
                 cardType.textContent = "master"
                 btn.disabled = false;
-            } else if (cardNumber.value.charAt(0) === "3" && cardNumber.value.length === 15 && (cardNumber.value.charAt(1) === "4" || cardNumber.value.charAt(1) === "7")){
-                    cardNumber.style.borderColor = "green";
-                    cardType.textContent = "AE";
-                    btn.disabled = false;
-            } else if(cardNumber.value.charAt(0) === "3" && cardNumber.value.length !== 15 && (cardNumber.value.charAt(1) === "4" || cardNumber.value.charAt(1) === "7")){
+            } else if (cardNumber.value.charAt(0) === "3" && cardNumber.value.length === 15 && (cardNumber.value.charAt(1) === "4" || cardNumber.value.charAt(1) === "7")) {
+                cardNumber.style.borderColor = "green";
+                cardType.textContent = "AE";
+                btn.disabled = false;
+            } else if (cardNumber.value.charAt(0) === "3" && cardNumber.value.length !== 15 && (cardNumber.value.charAt(1) === "4" || cardNumber.value.charAt(1) === "7")) {
                 cardNumber.style.borderColor = "red";
                 cardType.textContent = "";
                 btn.disabled = true;
-            } else if(cardNumber.value.length < 13){
+            } else if (cardNumber.value.length < 13) {
                 cardType.textContent = "";
                 btn.disabled = true;
             }
 
 
+        } else {
+            btn.disabled = true;
+            cardType.textContent = ""
+        }
     })
 
 });
