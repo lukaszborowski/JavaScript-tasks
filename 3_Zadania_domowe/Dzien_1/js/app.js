93bdd980a232a1c99ea57b92fedf834814486513
lukaszborowski <lukasz1borowski@gmail.com>
2023-03-09 20:02:00 +0100

    added mising line of code

--- 3_Zadania_domowe/Dzien_1/js/app.js
+++ 3_Zadania_domowe/Dzien_1/js/app.js
@@ -102,4 +102,5 @@ document.addEventListener("DOMContentLoaded", function() {
         getLastNumbers(2, [1,2,3,4,5,6,7]);
         getLastNumbers(4, [6,7,8,10,11,12,13,14,15])
         getLastNumbers('ala', [-4,-3,-2,0,1,2,3,4])
+        getLastNumbers([-4,-3,-2,0,1,2,3,4])
 });
