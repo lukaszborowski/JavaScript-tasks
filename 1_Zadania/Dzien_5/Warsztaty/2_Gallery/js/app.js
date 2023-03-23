ec96a3c78871b81be87a6627cb0312fbd6340ce8
lukaszborowski <lukasz1borowski@gmail.com>
2023-03-23 16:30:05 +0100

    Gallery

---
+++ 1_Zadania/Dzien_5/Warsztaty/2_Gallery/js/app.js
@@ -0,0 +1,38 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const galleryLinks = document.querySelectorAll(".gallery__link");
+    const lightBox = document.querySelector(".lightbox");
+    const showImage = document.querySelector(".lightbox__img");
+    const closeImg = document.querySelector(".lightbox__close");
+    const closeBack = document.querySelector(".lightbox__box");
+    console.log(galleryLinks);
+    console.log(lightBox);
+
+
+    for (let el of galleryLinks) {
+        el.addEventListener('click', function (e) {
+            showImage.src = this.getAttribute('href');
+            e.preventDefault();
+            lightBox.style.display = "flex";
+            console.log("działam");
+
+
+        });
+
+    }
+    closeImg.addEventListener('click', function (e) {
+        lightBox.style.display = "none";
+
+
+    });
+    lightBox.addEventListener('click', function (e) {
+        lightBox.style.display = "none";
+
+
+    });
+
+    closeBack.addEventListener('click', function (e) {
+        e.stopPropagation();
+    })
+});
